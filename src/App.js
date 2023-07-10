fe2ed7c9563fc16292fcabf003a04c852e910f35
mrmitcp1 <mrmitcp55@gmail.com>
2023-07-10 08:28:35 +0700

    expand

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {useState} from "react";
 
 function App() {
+  const [state,setState]=useState(false)
+  const toggleExpand = () =>{
+    setState(!state);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>Conditional Rendering</h1>
+        {!state ? (
+            <button onClick={toggleExpand}>Xem giới thiệu</button>
+        ) : (
+            <div>
+              <button onClick={toggleExpand}>Đóng giới thiệu</button>
+              <p>Nội dung giới thiệu</p>
+            </div>
+        )}
+      </div>
   );
 }
 
